a17569a4bc0e30b4e757d618795b623de2df5d30
Sylvestre Mergulhão <contato@mergulhao.info>
2013-01-15 02:07:04 -0200

    add puppet postgresql provision

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,6 +1,9 @@
 class { 'postgresql::server':
   config_hash => {
     'listen_addresses'           => '*',
+    'postgres_password'          => 'postgres',
+    'ip_mask_allow_all_users'    => '10.0.2.0/24',
+    'ip_mask_deny_postgres_user' => '0.0.0.0/32',
   },
 }
 
