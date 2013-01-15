ea4ea2ff418f1bcf49ac1aa769c9d432582e47a7
Sylvestre Mergulhão <contato@mergulhao.info>
2013-01-15 04:36:39 -0200

    add puppet recipes to install postgresql

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,10 +1,19 @@
-class { 'postgresql::server':
-  config_hash => {
-    'listen_addresses'           => '*',
-    'postgres_password'          => 'postgres',
-    'ip_mask_allow_all_users'    => '10.0.2.0/24',
-    'ip_mask_deny_postgres_user' => '0.0.0.0/32',
-  },
+class postgresql {
+  package { 'postgresql': ensure => installed }
+  service { 'postgresql':
+    ensure  => running,
+    require => Package['postgresql'],
+  }
+  file { 'pg_hba.conf':
+    path    => '/etc/postgresql/9.1/main/pg_hba.conf',
+    content => template("postgresql/pg_hba.conf.erb"),
+    notify  => Service['postgresql'],
+  }
+  file { 'postgresql.conf':
+    path    => '/etc/postgresql/9.1/main/postgresql.conf',
+    content => template("postgresql/postgresql.conf.erb"),
+    notify  => Service['postgresql'],
+  }
 }
 
-include postgresql::server
+include postgresql
